bf078b355fd87e5eeb0bd6e40141d231f528dcdc
williamnoonan <willnoonan@outlook.com>
2022-05-26 12:26:56 -0500

    copied latest boostrap html for a dropdown/button combo and converted to javascript. current buttons are just placeholders

--- map/static/map/js/map_index.js
+++ map/static/map/js/map_index.js
@@ -38,6 +38,63 @@ function CenterControl(controlDiv, map) {
   });
 }
 
+function RouteDropdown() {
+    /**
+     * This function creates and returns a div that contains a route-selection dropdown.
+     */
+
+    /*
+        The following html was copied from https://getbootstrap.com/docs/5.2/components/dropdowns/.
+        This was converted to javascript using the document.createElement method.
+     */
+    // <div id="route-dropdown" className="dropdown">
+    //     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+    //             data-bs-toggle="dropdown" aria-expanded="false">
+    //         Dropdown button
+    //     </button>
+    //     <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    //         <li><a className="dropdown-item" href="#">Action</a></li>
+    //         <li><a className="dropdown-item" href="#">Another action</a></li>
+    //         <li><a className="dropdown-item" href="#">Something else here</a></li>
+    //     </ul>
+    // </div>
+
+    const routeDropdown = document.createElement("div")
+    routeDropdown.id = "route-dropdown"
+    routeDropdown.className = "dropdown"
+    routeDropdown.style.marginTop = "10px";
+
+    const button = document.createElement("button")
+    button.className="btn btn-secondary dropdown-toggle"
+    button.type="button"
+    button.id = "dropdownMenuButton1"
+    button.setAttribute("data-bs-toggle", "dropdown")
+    button.setAttribute("aria-expanded", "false")
+    button.innerHTML = "Dropdown button"
+    routeDropdown.appendChild(button)
+
+    const listItems = document.createElement("ul")
+    listItems.className="dropdown-menu"
+    listItems.setAttribute("aria-labelledby", "dropdownMenuButton1")
+    // listItems.innerHTML = '<li><a className="dropdown-item" href="#">Action</a></li>'
+
+    const arr = ['alpha', 'bravo', 'charlie', 'delta', 'echo'];
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        const li = document.createElement('li');     // create li element.
+
+        const li_button = document.createElement("button")
+        li_button.type = "button"
+        li_button.setAttribute("class", 'dropdown-item')
+        li_button.innerHTML = arr[i]
+
+        li.appendChild(li_button);
+        listItems.appendChild(li);     // append li to ul.
+    }
+    routeDropdown.appendChild(listItems)
+    return routeDropdown;
+}
+
 
 
 function initMap() {
@@ -53,11 +110,18 @@ function initMap() {
         map: map,
     });
 
-  // Create the DIV to hold the control and call the CenterControl()
-  // constructor passing in this DIV.
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
     const centerControlDiv = document.createElement("div")
     CenterControl(centerControlDiv, map)
+
+    // Create the DIV to hold the route-selection dropdown.
+    const routeDropdownDiv = RouteDropdown();
+
+    // Push the divs to the map.
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv)
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(routeDropdownDiv)
+
 }
 
 window.initMap = initMap;
