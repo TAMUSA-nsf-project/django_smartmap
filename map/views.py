48ac2f3c1d218d3d0776dddeb812fbc972ecb5cf
willnoonan <willnoonan@outlook.com>
2022-05-27 11:19:33 -0500

    json file is read in views.py and passed to template

--- map/views.py
+++ map/views.py
@@ -1,10 +1,19 @@
+import os
+
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from django.conf import settings
+import json
 
 import socketio
 sio = socketio.Server(async_mode='threading')
 
+# Read json file with all the route data (bus stops and their lat, lng, etc)
+with open(os.path.join(settings.BASE_DIR, "route_data/allRoutes.json")) as f:
+    json_data = json.load(f)
+
+# to be passed to template
+json_routes = json.dumps(json_data)
 
 # Create your views here.
 
@@ -21,6 +30,7 @@ def map_hello_world(request):
         "google_api_key": settings.GOOGLE_API_KEY,
         "lat_coord": 29.4190,
         "lng_coord": -98.4836,
+        "route_json": json_routes
     }
     return render(request, 'map/map_index.html', context)
 
