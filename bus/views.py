0899702a16774dc4f6253c5812919c00e0049e94
willnoonan <willnoonan@outlook.com>
2022-06-03 13:02:19 -0500

    added estimated arrival time calculations

--- bus/views.py
+++ bus/views.py
@@ -32,9 +32,43 @@ def busdriver_view(request):
     return render(request, "bus/busdriver_2.html", context)
 
 
+
+
+import googlemaps
+from datetime import datetime
+from googlemaps.distance_matrix import distance_matrix
+
+
+# updates json_data dict as well
+def calc_est_arrival_times(data):
+    # data format: {'selected_route': str, 'bus_lat': float, 'bus_lng': float }
+
+    bus_route = data["selected_route"]
+    stop_data = json_data[bus_route]
+    stops_latlng = [(stop["Lat"], stop["Lng"]) for stop in stop_data]
+
+    gmaps = googlemaps.Client(key=settings.GOOGLE_PYTHON_API_KEY)
+    res = distance_matrix(gmaps, origins=(data['bus_lat'], data['bus_lng']), destinations=stops_latlng, transit_mode="bus",
+                          departure_time=datetime.now())
+
+    for i in range(len(stop_data)):
+        stop_data[i]['Est Arrival'] = res['rows'][0]['elements'][i]['duration']['text']
+
+    return {bus_route: stop_data}
+
+
+
+
+
 @sio.event
 def broadcast_bus(sid, data):
     # current assumptions: sid does not change for a unique client even when selected route changes (so far I've observed
     # this to be true)
+    # data format: {'selected_route': str, 'bus_lat': float, 'bus_lng': float }
     busses[sid] = data
     sio.emit("display busses", busses)
+    res = calc_est_arrival_times(data)
+    sio.emit("updated arrival times", res)
+
+
+# in new socket func, data comes in, gets sent to some functions for processing and a new set of data is sent out
