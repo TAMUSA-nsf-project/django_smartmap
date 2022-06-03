8319db99b3efc15338b38e1c396ea4182543b2cd
willnoonan <willnoonan@outlook.com>
2022-06-03 15:46:23 -0500

    updated calc_est_arrival_times to get around Distance Matrix API usage limit of 25 destinations, very slow (needs parallel computing)

--- bus/distancematrixcalcs.py
+++ bus/distancematrixcalcs.py
@@ -1,15 +1,14 @@
 import googlemaps
 from datetime import datetime
 from googlemaps.distance_matrix import distance_matrix
-
-
 from django.conf import settings
+
 
 # updates json_data dict as well
 def calc_est_arrival_times(socket_data, json_data):
     """
-    TODO Figure out way around Distance Matrix API usage limit: Maximum of 25 origins or 25 destinations per request
-    Routes such as 'Route 51' have more than 25 stops and will cause error: "googlemaps.exceptions.ApiError: MAX_DIMENSIONS_EXCEEDED"
+    This contains a way of bypassing the Distance Matrix API usage limit of 25 destinations by passing only
+    one destination at a time. It's very slow without parallel computing.
     """
     # data format: {'selected_route': str, 'bus_lat': float, 'bus_lng': float }
 
@@ -17,11 +16,28 @@ def calc_est_arrival_times(socket_data, json_data):
     stop_data = json_data[bus_route]
     stops_latlng = [(stop["Lat"], stop["Lng"]) for stop in stop_data]
 
+    timenow = datetime.now()
+
     gmaps = googlemaps.Client(key=settings.GOOGLE_PYTHON_API_KEY)
-    res = distance_matrix(gmaps, origins=(socket_data['bus_lat'], socket_data['bus_lng']), destinations=stops_latlng, transit_mode="bus",
-                          departure_time=datetime.now())
+    res = [distance_matrix(gmaps, origins=(socket_data['bus_lat'], socket_data['bus_lng']), destinations=stop, transit_mode="bus",
+                          departure_time=timenow) for stop in stops_latlng]
 
     for i in range(len(stop_data)):
-        stop_data[i]['est_arrival'] = res['rows'][0]['elements'][i]['duration']['text']
+        stop_data[i]['est_arrival'] = res[i]['rows'][0]['elements'][0]['duration']['text']
 
     return {bus_route: stop_data}
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
