b0ec337063aa0906bb1309d69b32dc7e2975cacb
willnoonan <willnoonan@outlook.com>
2022-06-20 19:33:26 -0500

    fixed wsgi bugs where the wrong project was referenced in the environment variable operation and the settings were being imported (via map.views) before it

--- django_smartmap/wsgi.py
+++ django_smartmap/wsgi.py
@@ -4,17 +4,20 @@ WSGI config for django_smartmap project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-import socketio
-from map.views import sio
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_smartmap.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_transit.settings')
+# I believe that the settings must be imported after the env var operation above
+from django.conf import settings
+import socketio
+
+# Socket IO
+sio = settings.SIO
 
 django_app = get_wsgi_application()
 application = socketio.WSGIApp(sio, django_app)
