8aa831723f54670b66fdd0619bdb4a79a5fa9e7a
willnoonan <willnoonan@outlook.com>
2022-06-21 13:05:03 -0500

    added cloud run environment variables

--- manage.py
+++ manage.py
@@ -7,6 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_smartmap.settings')
+
+    # GCP Cloud Run environment variables
+    os.environ.setdefault('GOOGLE_CLOUD_PROJECT', 'nsf-2131193-18902')
+    os.environ.setdefault('USE_CLOUD_SQL_AUTH_PROXY', 'true')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
