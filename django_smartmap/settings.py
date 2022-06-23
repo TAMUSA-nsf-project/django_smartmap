b5ca88be2cc8f05d134c600d0811be76717b3f92
Anoop Abraham <anoopabraham25@gmail.com>
2022-06-23 13:41:13 -0500

    Using sslserver package to serve app over https

--- django_smartmap/settings.py
+++ django_smartmap/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # 'django_extensions',
+    'sslserver',
     'django.contrib.staticfiles',  # for javascript, others
 ]
 
