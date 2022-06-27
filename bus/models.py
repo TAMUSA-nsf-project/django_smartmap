27dae63c5598712071a54544f37fe0dfc9733e70
willnoonan <willnoonan@outlook.com>
2022-06-27 15:44:32 -0500

    added bus driver model

--- bus/models.py
+++ bus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class BusDriver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("access_busdriver_pages", "Can access bus driver pages.")
+        ]
+
+    def __str__(self):
+        return self.user.username
