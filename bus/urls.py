d9f4bc15daceafbd6a34578611be12f87434896c
willnoonan <willnoonan@outlook.com>
2022-06-27 11:26:28 -0500

    Replaced usage of socket.io in template, which sends bus position data to server, with jquery ajax. Replaced server-side socket.io event handler with custom ajax-handler function. Deleted usage of distancematrixcalcs.py, which may be replaced.

--- bus/urls.py
+++ bus/urls.py
@@ -6,6 +6,7 @@ app_name = "bus"
 urlpatterns = [
     # Bus driver
     path('busdriver/', views.busdriver_view, name='busdriver-page'),
+    path('busposition-ajax/', views.bus_position_ajax, name='busposition-ajax'),
 
     # Simulation file
     path('simulation/', views.simulation_view, name='simulation-page'),
