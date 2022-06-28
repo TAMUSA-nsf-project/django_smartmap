4c2b34ffb969f8abf32229cafacc1df3747a916d
willnoonan <willnoonan@outlook.com>
2022-06-28 14:44:00 -0500

    initial bus-route related models

--- bus/admin.py
+++ bus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BusStop, BusRoute, BusRouteStop
+
+admin.site.register(BusStop)
+admin.site.register(BusRoute)
+admin.site.register(BusRouteStop)
--- bus/models.py
+++ bus/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BusStop(models.Model):
+    stop_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class BusRoute(models.Model):
+    # route_id = models.IntegerField()  # don't need, not in json, django auto creates an id field
+    name = models.CharField(max_length=200)
+    first_stop = models.ForeignKey("BusStop", on_delete=models.CASCADE, related_name="first_stop")
+    last_stop = models.ForeignKey("BusStop", on_delete=models.CASCADE, related_name="last_stop")
+    active = False
+
+    def __str__(self):
+        return self.name
+
+
+class BusRouteStop(models.Model):
+    parent_route = models.ForeignKey("BusRoute", on_delete=models.CASCADE)
+    bus_stop = models.ForeignKey("BusStop", on_delete=models.CASCADE)
+    route_index = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.parent_route.name}::{self.bus_stop.name}"
