e903f385e59e7d72d6ac1795a63ad7709f66246a
willnoonan <willnoonan@outlook.com>
2022-07-13 15:25:58 -0500

    added check for existence of admin user

--- main/migrations/0002_initial.py
+++ main/migrations/0002_initial.py
@@ -76,24 +76,26 @@ def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> Non
     Dynamically create an admin user as part of a migration
     Password is pulled from Secret Manger.
     """
-    if os.getenv("GOOGLE_CLOUD_PROJECT", None) is None:
-        # We are in DEV
-        admin_password = "P@ssword1"
-    else:
-        client = secretmanager.SecretManagerServiceClient()
+    if not User.objects.filter(username="admin").exists():
 
-        # Get project value for identifying current context
-        _, project = google.auth.default()
+        if os.getenv("GOOGLE_CLOUD_PROJECT", None) is None:
+            # We are in DEV
+            admin_password = "P@ssword1"
+        else:
+            client = secretmanager.SecretManagerServiceClient()
 
-        # Retrieve the previously stored admin password
-        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
-        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
-        admin_password = client.access_secret_version(name=name).payload.data.decode(
-            "UTF-8"
-        )
+            # Get project value for identifying current context
+            _, project = google.auth.default()
 
-    # Create a new user using acquired password, stripping any accidentally stored newline characters
-    User.objects.create_superuser("admin", password=admin_password.strip())
+            # Retrieve the previously stored admin password
+            PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
+            name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
+            admin_password = client.access_secret_version(name=name).payload.data.decode(
+                "UTF-8"
+            )
+
+        # Create a new user using acquired password, stripping any accidentally stored newline characters
+        User.objects.create_superuser("admin", password=admin_password.strip())
 
 
 class Migration(migrations.Migration):
