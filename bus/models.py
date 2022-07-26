e8029ee25e01529abfaa47c3dffdb4ca681c04c5
willnoonan <willnoonan@outlook.com>
2022-07-26 17:27:56 -0500

    display date_added in admin page TransitLog string representation

--- bus/models.py
+++ bus/models.py
@@ -77,7 +77,7 @@ class TransitLog(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.bus_route.name}: {self.driver}"
+        return f"{self.bus_route.name}, {self.date_added}, {self.driver}"
 
 
 class TransitLogEntry(models.Model):
