e031349311987e038b9258c585b92ca9289af049
Anoop Abraham <anoopabraham25@gmail.com>
2022-08-23 14:36:30 -0500

    Adding the default settings file to wsgi file.

--- django_smartmap/wsgi.py
+++ django_smartmap/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-
+import os
 from django.core.wsgi import get_wsgi_application
 
+# Setting the default settings file
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_smartmap.settings.production')
 application = get_wsgi_application()
