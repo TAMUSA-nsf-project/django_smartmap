a4360a1e499e472605de63e808e7680fefdd3606
willnoonan <willnoonan@outlook.com>
2022-09-08 11:21:45 -0500

    use new form and create Profile object when new user registers

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserProfileForm
+from .models import Profile
 
 
 # Create your views here.
@@ -10,12 +11,17 @@ def register(request):
     """Register a new user."""
     if request.method != 'POST':
         # Display blank registration form
-        form = UserCreationForm()
+        form = UserProfileForm()
     else:
         # Process completed form
-        form = UserCreationForm(data=request.POST)
+        form = UserProfileForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
+
+            # Create Profile object
+            phone = request.POST.get('phone')
+            Profile.objects.create(phone_number=phone, user=new_user)
+
             # Log the user in and redirect to home page
             login(request, new_user)
             return redirect('main:index')
