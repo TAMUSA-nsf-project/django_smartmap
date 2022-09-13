1a1475b1170b28899890e915ff81a3ed56d33e7f
willnoonan <willnoonan@outlook.com>
2022-09-13 10:30:54 -0500

    create model form for Profile model

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,15 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
 from django import forms
 
+from .models import Profile
 
-class UserProfileForm(UserCreationForm):
+
+class ProfileRegisterForm(forms.ModelForm):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits "
                                          "allowed.")
     phone = forms.CharField(validators=[phone_regex], max_length=17)
 
     class Meta:
-        model = User
-        fields = ['username', 'phone', 'password1', 'password2']
+        model = Profile
+        fields = ['phone']
