d7d3677d3c6b2d03b499630acade253e1546c9cd
Anoop Abraham <anoopabraham25@gmail.com>
2022-09-14 11:30:09 -0500

    Minor changes after code review.

--- users/models.py
+++ users/models.py
@@ -13,12 +13,6 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-# Todo Merge Williams changes with signals
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#         instance.profile.save()
 
 
 class PreferredRoutes(models.Model):
