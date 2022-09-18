95a793ed874153a7f316c03cfee25b04a0858c00
willnoonan <willnoonan@outlook.com>
2022-09-18 10:27:12 -0500

    completed Twilio SMS feature: when an Announcement is created, post_save signal sends SMS via Twilio to Profile instances containing a phone number

--- communications/signals.py
+++ communications/signals.py
@@ -5,9 +5,13 @@ from django.dispatch import receiver
 from django.conf import settings
 from twilio.rest import Client
 
+from users.models import Profile
+
+# Get Twilio env variables
 twilio_account_sid = settings.TWILIO_ACCOUNT_SID
 twilio_auth_token = settings.TWILIO_AUTH_TOKEN
 
+# Create a Twilio client
 twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
 
@@ -17,9 +21,16 @@ def send_announcement_as_sms(sender, instance, created, **kwargs):
     Sends an announcement as a text message to users.
     """
     if created:
-        message = twilio_client.messages.create(
-            messaging_service_sid='MG1a63f57041a68c7c2d88222d8d22af67',
-            body=instance.text,
-            to='+1'
-        )
-        print(f"Announcement: {instance}")
+        # Get profiles with a phone number (exclude those without)
+        profiles_with_phone = Profile.objects.exclude(phone="")
+
+        # TODO is the following blocking, do we need to setup async task-scheduling? Possibly use Django Q
+        for profile in profiles_with_phone:
+            message = twilio_client.messages.create(
+                messaging_service_sid='MG1a63f57041a68c7c2d88222d8d22af67',
+                body=instance.text,
+                to=str(profile.phone)
+            )
+
+        if profiles_with_phone:
+            print(f"announcement sent via SMS: '{instance}'")
