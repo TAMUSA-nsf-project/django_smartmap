0c1615fc6228f6d342b93aa8f6a3df3fd3089ec7
willnoonan <willnoonan@outlook.com>
2022-09-24 13:46:03 -0500

    delete unused CenterControl function

--- map/static/map/js/map_index.js
+++ map/static/map/js/map_index.js
@@ -11,49 +11,6 @@ const defaultTimeString = "TBD";
 let poly, left, right;
 let directionsService;
 
-/**
- * The CenterControl adds a control to the map that recenters the map on marker_coords
- * This constructor takes the control DIV as an argument.
- * 4 Aug 2022: no longer being displayed, TODO delete?
- */
-function CenterControl(map) {
-    // Create the DIV to hold the control and call the CenterControl()
-    // constructor passing in this DIV.
-    const controlDiv = document.createElement("div")
-
-
-    // Set CSS for the control border.
-    const controlUI = document.createElement("div");
-
-    controlUI.style.backgroundColor = "#fff";
-    controlUI.style.border = "2px solid #fff";
-    controlUI.style.borderRadius = "3px";
-    controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
-    controlUI.style.cursor = "pointer";
-    controlUI.style.marginTop = "8px";
-    controlUI.style.marginBottom = "22px";
-    controlUI.style.textAlign = "center";
-    controlUI.title = "Click to recenter the map";
-    controlDiv.appendChild(controlUI);
-
-    // Set CSS for the control interior.
-    const controlText = document.createElement("div");
-
-    controlText.style.color = "rgb(25,25,25)";
-    controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-    controlText.style.fontSize = "16px";
-    controlText.style.lineHeight = "38px";
-    controlText.style.paddingLeft = "5px";
-    controlText.style.paddingRight = "5px";
-    controlText.innerHTML = "Center Map";
-    controlUI.appendChild(controlText);
-    // Setup the click event listeners: simply set the map to marker_coords.
-    controlUI.addEventListener("click", () => {
-        map.setCenter(MAP_CENTER);
-    });
-
-    return controlDiv;
-}
 
 function RouteDropdown(map) {
     /**
