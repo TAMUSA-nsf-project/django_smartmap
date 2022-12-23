532b7bc08042d86cd229ec427ef2a5ddb56a1bd0
willnoonan <willnoonan@outlook.com>
2022-12-23 11:09:18 -0600

    create services file for async functions, temp solution

---
+++ bus/services.py
@@ -0,0 +1,12 @@
+
+
+def logBusArrivalTimes(bus):
+    # get bus details obj
+    bus_route_details = bus.getBusRouteDetails()
+
+    # get nearest bus stop
+
+    # get bus stops ahead of bus
+
+
+
